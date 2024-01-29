77769c451f1eb7cd9e01c392a3262b99b122309d
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2024-01-28 23:00:59 -0700

    Wrote JsonWriter Library

---
+++ custom_libraries/json_writer.cc
@@ -0,0 +1,131 @@
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include "json_writer.h"
+
+namespace rose {
+
+JsonWriter::JsonWriter(std::ostream &out) : out_(out) {
+  level_ = 0;
+  level_empty_ = true;
+}
+
+void JsonWriter::BeginObject() { BeginLevel("{"); }
+
+void JsonWriter::BeginObject(const std::string &name) {
+  std::stringstream beginner;
+  beginner << '"' << name << "\": {";
+  BeginLevel(beginner.str());
+}
+
+void JsonWriter::BeginArray() { BeginLevel("["); }
+
+void JsonWriter::BeginArray(const std::string &name) {
+  std::stringstream beginner;
+  beginner << '"' << name << "\": [";
+  BeginLevel(beginner.str());
+}
+
+void JsonWriter::CreateInt(int value) { CreateItem(std::to_string(value)); }
+
+void JsonWriter::CreateInt(const std::string &name, int value) {
+  CreateProperty(name, std::to_string(value));
+}
+
+void JsonWriter::CreateFloat(float value) { CreateItem(std::to_string(value)); }
+
+void JsonWriter::CreateFloat(const std::string &name, float value) {
+  CreateProperty(name, std::to_string(value));
+}
+
+void JsonWriter::CreateDouble(double value) {
+  CreateItem(std::to_string(value));
+}
+
+void JsonWriter::CreateDouble(const std::string &name, double value) {
+  CreateProperty(name, std::to_string(value));
+}
+
+void JsonWriter::CreateString(const std::string &value) {
+  // Having unescaped quotation marks inside a string messes up the formatting,
+  // so this is a required work-around until std::format wants to play nice.
+  if (value.contains('"')) {
+    CreateItem(value);
+    return;
+  }
+  std::stringstream sout;
+  sout << '"' << value << '"';
+  CreateItem(sout.str());
+}
+
+void JsonWriter::CreateString(const std::string &name,
+                              const std::string &value) {
+  // Having unescaped quotation marks inside a string messes up the formatting,
+  // so this is a required work-around until std::format wants to play nice.
+  if (value.contains('"')) {
+    CreateProperty(name, value);
+    return;
+  }
+  std::stringstream sout;
+  sout << '"' << value << '"';
+  CreateProperty(name, sout.str());
+}
+
+void JsonWriter::CreateBool(bool value) {
+  CreateItem(value ? "true" : "false");
+}
+
+void JsonWriter::CreateBool(const std::string &name, bool value) {
+  CreateProperty(name, value ? "true" : "false");
+}
+
+void JsonWriter::CreateNull() { CreateItem("null"); }
+
+void JsonWriter::CreateNull(const std::string &name) {
+  CreateProperty(name, "null");
+}
+
+void JsonWriter::EndObject() { EndLevel('}'); }
+
+void JsonWriter::EndArray() { EndLevel(']'); }
+
+void JsonWriter::BeginLevel(const std::string &beginner) {
+  if (!level_empty_) out_ << ',';
+  if (level_) out_ << '\n';  // Prevents writing a newline as the first char.
+  Indent();
+  out_ << beginner;
+  ++level_;
+  level_empty_ = true;
+}
+
+void JsonWriter::EndLevel(char ender) {
+  --level_;
+  if (!level_empty_) {
+    out_ << '\n';
+    Indent();
+  }
+  out_ << ender;
+  level_empty_ = false;
+}
+
+void JsonWriter::CreateItem(const std::string &value) {
+  if (!level_empty_) out_ << ',';
+  out_ << '\n';
+  Indent();
+  out_ << value;
+  level_empty_ = false;
+}
+
+void JsonWriter::CreateProperty(const std::string &name,
+                                const std::string &value) {
+  if (!level_empty_) out_ << ',';
+  out_ << '\n';
+  Indent();
+  out_ << '"' << name << "\": " << value;
+  level_empty_ = false;
+}
+
+void JsonWriter::Indent() { out_ << std::string((level_ << 1), ' '); }
+
+}  // namespace rose
---
+++ custom_libraries/json_writer.h
@@ -0,0 +1,72 @@
+#include <ostream>
+#include <string>
+
+#ifndef CS2420_CUSTOMLIBRARIES_JSONWRITER_H_
+#define CS2420_CUSTOMLIBRARIES_JSONWRITER_H_
+
+namespace rose {
+
+// Provides a user-friendly interface to write JSON data to a std::ostream.
+class JsonWriter {
+ public:
+  JsonWriter(std::ostream &out);
+
+  // Writes an opening curly bracket.
+  void BeginObject();
+  // Begins an object property named `name`.
+  void BeginObject(const std::string &name);
+  // Writes an opening square bracket.
+  void BeginArray();
+  // Begins an array property named `name`.
+  void BeginArray(const std::string &name);
+  // Writes an int item with value `value`.
+  void CreateInt(int value);
+  // Writes an int property named `name` with value `value`.
+  void CreateInt(const std::string &name, int value);
+  // Writes a float item with value `value`.
+  void CreateFloat(float value);
+  // Writes a float property named `name` with value `value`.
+  void CreateFloat(const std::string &name, float value);
+  // Writes a double item with value `value`.
+  void CreateDouble(double value);
+  // Writes a double property named `name` with value `value`.
+  void CreateDouble(const std::string &name, double value);
+  // Writes a string item with value `value`.
+  void CreateString(const std::string &value);
+  // Writes a string property named `name` with value `value`.
+  void CreateString(const std::string &name, const std::string &value);
+  // Writes a bool item with value `value`.
+  void CreateBool(bool value);
+  // Writes a bool property named `name` with value `value`.
+  void CreateBool(const std::string &name, bool value);
+  // Writes a null item.
+  void CreateNull();
+  // Writes a null property named `name`.
+  void CreateNull(const std::string &name);
+  // Writes a closing curly bracket.
+  void EndObject();
+  // Writes a closing square bracket.
+  void EndArray();
+
+ private:
+  // Begins a new level with the string `beginner`.
+  void BeginLevel(const std::string &beginner);
+  // Ends a level with the char `ender`.
+  void EndLevel(char ender);
+  // Writes an item with its value represented as a string.
+  void CreateItem(const std::string &value);
+  // Writes a property with its value represented as a string.
+  void CreateProperty(const std::string &name, const std::string &value);
+  // Inserts two spaces per level.
+  void Indent();
+
+  // The output stream being written to.
+  std::ostream &out_;
+  // The indentation level.
+  int level_;
+  bool level_empty_;
+};
+
+}  // namespace rose
+
+#endif  // CS2420_CUSTOMLIBRARIES_JSONWRITER_H_
